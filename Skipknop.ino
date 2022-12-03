56352517da2da0988f50bb35920c95a7549935f8
Niek Blankers <niek@blankers.eu>
2022-12-03 14:32:26 +0100

    Nieuwe espnow broadcast mode

--- Skipknop.ino
+++ Skipknop.ino
@@ -1,24 +1,20 @@
-#include <stdint.h>
-#include <stdio.h>
-
 #include "Arduino.h"
-#include "WifiEspNow.h"
+#include <espnow.h>
 #include "ESP8266WiFi.h"
-#include "EEPROM.h"
 
 // ==================================================================
 // Skipbutton things
 
 const uint8_t numButtons = 3;
-const uint8_t buttonPins[] = {D5, D1, D2}; // Skip, volup, voldown
+const uint8_t buttonPins[] = { D5, D1, D2 };  // Skip, volup, voldown
 
-bool buttonStates[] = {false, false, false};
+bool buttonStates[] = { false, false, false };
 bool noButtonsPressed = true;
 
 // Skip: revspace/button/skip - jump_fwd
 // Stop: revspace/button/stop
 
-#define MQTT_TOPIC_BASE "revspace/button/"
+#define MQTT_TOPIC_BASE "#Urevspace/button/"
 #define TOPIC_SKIP "skip"
 #define TOPIC_VOL_UP "vol_up"
 #define TOPIC_VOL_DN "vol_down"
@@ -28,28 +24,30 @@ bool noButtonsPressed = true;
 #define MQTT_MSG_SKIP "jump_fwd"
 #define MQTT_MSG "hackalot"
 
-#define ACTION_NOTHING 0
-#define ACTION_SKIP 1
-#define ACTION_VOL_UP 2
-#define ACTION_VOL_DOWN 3
-#define ACTION_SHUFFLE 4
-#define ACTION_STOP 5
+typedef enum {
+  ACTION_NOTHING,
+  ACTION_SKIP,
+  ACTION_VOL_UP,
+  ACTION_VOL_DOWN,
+  ACTION_SHUFFLE,
+  ACTION_STOP
+} action_t;
 
-uint8_t action = ACTION_NOTHING;
-
-// ==================================================================
-// ESP-Now Things
-
-static const char AP_NAME[] = "revspace-espnow";
+static action_t action = ACTION_NOTHING;
 
 typedef enum {
   E_SEND,
-  E_ACK,
-  E_DISCOVER,
+  E_DONE,
   E_SLEEP
 } skip_mode_t;
 
 static skip_mode_t mode = E_SEND;
+
+// ==================================================================
+// ESP-Now Things
+
+#define ESP_NOW_CHANNEL 9
+uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 
 // ==================================================================
 // Cut here ✂️------------------------------------------------------
@@ -68,8 +66,8 @@ bool buttonHeld(uint8_t butt, unsigned long timeout = 1250UL) {
 void doShutdown() {
   // Disable power
   digitalWrite(D0, LOW);
-  
-  while(true) {
+
+  while (true) {
     delay(10);
   }
 }
@@ -80,7 +78,7 @@ void setup() {
   digitalWrite(D0, HIGH);
 
   // initialise pins
-  for(uint8_t i = 0; i < numButtons; i++) {
+  for (uint8_t i = 0; i < numButtons; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
 
@@ -88,10 +86,10 @@ void setup() {
   delay(5);
 
   // Read pins
-  for(uint8_t i = 0; i < numButtons; i++) {
+  for (uint8_t i = 0; i < numButtons; i++) {
     bool state = !digitalRead(buttonPins[i]);
     buttonStates[i] = state;
-    
+
     if (state)
       noButtonsPressed = false;
   }
@@ -105,10 +103,24 @@ void setup() {
     doShutdown();
   }
 
-  WifiEspNow.begin();
-  EEPROM.begin(512);  
+  // Initialize esp-now on right channel
+  WiFi.forceSleepWake();
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("aapnoot", "miesmies", ESP_NOW_CHANNEL, NULL, false);
+  delay(10);
+  WiFi.disconnect();
+  wifi_set_channel(ESP_NOW_CHANNEL);
 
-  if (buttonStates[0]) { // Main dome button
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    doShutdown();
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, ESP_NOW_CHANNEL, NULL, 0);
+
+  if (buttonStates[0]) {  // Main dome button
     if (!buttonHeld(0))
       action = ACTION_SKIP;
     else
@@ -116,155 +128,82 @@ void setup() {
     return;
   }
 
-  if (buttonStates[1]) { // Top button
+  if (buttonStates[1]) {  // Top button
     action = ACTION_VOL_UP;
     return;
   }
 
-  if (buttonStates[2]) { // Bottom button
+  if (buttonStates[2]) {  // Bottom button
     action = ACTION_VOL_DOWN;
     return;
   }
 }
 
 void loop() {
-  WifiEspNowSendStatus status;
-  struct WifiEspNowPeerInfo recv;
-  char line[128];
 
   switch (mode) {
     case E_SEND:
-        // read last known receiver info from EEPROM
-        EEPROM.get(0, recv);
-        if (valid_peer(&recv)) {
-            // send SKIP message to last known address
-            sprintf(line, "Sending SKIP to %02X:%02X:%02X:%02X:%02X:%02X (chan %d)...",
-                    recv.mac[0], recv.mac[1], recv.mac[2], recv.mac[3], recv.mac[4], recv.mac[5], recv.channel);
-            Serial.print(line);
-  
-            WifiEspNow.addPeer(recv.mac, recv.channel, nullptr);
+      switch (action) {
+        case ACTION_SKIP:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_SKIP, MQTT_MSG_SKIP);  // Different message for backwards-compatibility
+          break;
+        case ACTION_VOL_UP:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_VOL_UP, MQTT_MSG);
+          break;
+        case ACTION_VOL_DOWN:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_VOL_DN, MQTT_MSG);
+          break;
+        case ACTION_SHUFFLE:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_SHUF, MQTT_MSG);
+          break;
+        case ACTION_STOP:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_STOP, MQTT_MSG);
+          break;
+        default:
+          send_topic_text(MQTT_TOPIC_BASE TOPIC_SKIP, MQTT_MSG_SKIP);
+          break;
+      }
+      // Move to next section to check for held buttons
+      delay(10);
+      mode = E_DONE;
+      break;
 
-            switch (action) {
-              case ACTION_SKIP:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_SKIP, MQTT_MSG_SKIP); // Different message for backwards-compatibility
-                break;
-              case ACTION_VOL_UP:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_VOL_UP, MQTT_MSG);
-                break;
-              case ACTION_VOL_DOWN:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_VOL_DN, MQTT_MSG);
-                break;
-              case ACTION_SHUFFLE:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_SHUF, MQTT_MSG);
-                break;
-              case ACTION_STOP:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_STOP, MQTT_MSG);
-                break;
-              default:
-                send_topic_text(recv.mac, MQTT_TOPIC_BASE TOPIC_SKIP, MQTT_MSG_SKIP);
-                break;
-            }
-            
-  
-            mode = E_ACK;
-        } else {
-            mode = E_DISCOVER;
-        }
-        break;
-  
-    case E_ACK:
-        // wait for tx ack
-        status = WifiEspNow.getSendStatus();
-        switch (status) {
-          case WifiEspNowSendStatus::NONE:
-              if (millis() > 3000) {
-                  Serial.println("TX ack timeout");
-                  mode = E_DISCOVER;
-              }
-              break;
-          case WifiEspNowSendStatus::OK:
-              Serial.println("TX success");
-              mode = E_SLEEP;
-              switch (action) {
-                case ACTION_VOL_UP:
-                  if (buttonHeld(1, 900))
-                    mode = E_SEND;
-                break;
-                case ACTION_VOL_DOWN:
-                  if (buttonHeld(2, 900))
-                    mode = E_SEND;
-                break;
-                case ACTION_STOP:
-                  if (buttonHeld(0, 2500)) {
-                    action = ACTION_SHUFFLE;
-                    mode = E_SEND;
-                  }
-                break;
-              }
-              break;
-          case WifiEspNowSendStatus::FAIL:
-          default:
-              Serial.println("TX failed");
-              mode = E_DISCOVER;
-              break;
-        }
-        break;
-  
-    case E_DISCOVER:
-        Serial.println("Discovering master ...");
-        if (find_ap(AP_NAME, &recv)) {
-            // save it in EEPROM
-            sprintf(line, "found '%s' at %02X:%02X:%02X:%02X:%02X:%02X (chan %d), saving to EEPROM", AP_NAME,
-                recv.mac[0], recv.mac[1], recv.mac[2], recv.mac[3], recv.mac[4], recv.mac[5], recv.channel);
-            Serial.println(line);
-            EEPROM.put(0, recv);
-            EEPROM.end();
-        } else {
-            Serial.println("no master found!");
-        }
-        mode = E_SLEEP;
-        break;
-  
+    case E_DONE:
+      // First send done
+      mode = E_SLEEP;
+      switch (action) {
+        case ACTION_VOL_UP:
+          if (buttonHeld(1, 900))
+            mode = E_SEND;
+          break;
+        case ACTION_VOL_DOWN:
+          if (buttonHeld(2, 900))
+            mode = E_SEND;
+          break;
+        case ACTION_STOP:
+          if (buttonHeld(0, 2500)) {
+            action = ACTION_SHUFFLE;
+            mode = E_SEND;
+          }
+          break;
+      }
+      break;
+
     case E_SLEEP:
     default:
-        Serial.print("Shutting down...");
-        doShutdown();
-        break;
-    }  
+      delay(5);
+      Serial.print("Shutting down...");
+      doShutdown();
+      break;
+  }
 }
 
 // ==================================================================
 // ESP-Now Functions
 
-static bool find_ap(const char *name, struct WifiEspNowPeerInfo *peer)
-{
-    // scan for networks and try to find our AP
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-
-    int n = WiFi.scanNetworks();
-    for (int i = 0; i < n; i++) {
-        Serial.println(WiFi.SSID(i));
-        if (strcmp(name, WiFi.SSID(i).c_str()) == 0) {
-            // copy receiver data
-            peer->channel = WiFi.channel(i);
-            memcpy(peer->mac, WiFi.BSSID(i), sizeof(peer->mac));
-            return true;
-        }
-    }
-    // not found
-    return false;
+bool send_topic_text(const char *topic, const char *text) {
+  uint8_t *mac = broadcastAddress;
+  char buf[250];
+  int n = snprintf(buf, sizeof(buf), "%s %s", topic, text);
+  return esp_now_send(mac, (uint8_t *)buf, n) == 0;
 }
-
-static void send_topic_text(uint8_t *mac, const char *topic, const char *text)
-{
-    char buf[250];
-    int n = snprintf(buf, sizeof(buf), "%s %s", topic, text);
-    WifiEspNow.send(mac, (uint8_t *)buf, n);
-}
-
-static bool valid_peer(struct WifiEspNowPeerInfo *peer)
-{
-    return (peer->channel >= 1) && (peer->channel <= 14); 
-}
-
